f085aadf5fdd41a1a9fd29f1b98602b5c93f0a3e
miriamYom <m0527669018@gmail.com>
2023-12-06 13:07:37 +0200

    Change endpoint

--- client/src/RetirementSimulator/Calculation.js
+++ client/src/RetirementSimulator/Calculation.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { removePensionType } from "../redux/actions/employeeAction";
 import { getToken } from "./TokenService.js";
 import { userUrl } from "./endpoints";
+
 function Calculation() {
     const dispatch = useDispatch();
     let [result, setResult] = useState({});
@@ -11,13 +12,12 @@ function Calculation() {
     const userUrl = "http://localhost:5170/user/"
 
     useEffect(() => {
-        // dispatch(removePensionType());
         console.log(employeeDetails.pensionType);
-        console.log(employeeDetails);
+        console.log("employeeDetails: ",employeeDetails);
         var config = {
             headers: { Authorization: `Bearer ${getToken()}` }
         };
-        axios.post(`${userUrl}GetPensionCalculates?pensionType=${employeeDetails.pensionType}`,
+        axios.post(`${userUrl}GetPensionCalculates`,
             employeeDetails, config)
             .then(response => setResult((response.data)))
             .catch(error => console.error('There was an error!\n', error))
